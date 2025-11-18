987f6da2879f47dcc02f0c79a15f409e95e3be50
senamatsuda <sena_m_1007@yahoo.co.jp>
2025-11-18 17:32:08 +0900

    api

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // を apiBaseUrl に入力してください。空文字の場合はブラウザの LocalStorage のみを利用します。
 window.PA_SHIFT_CONFIG = Object.assign(
   {
-    apiBaseUrl: "https://student-plaza-pa-shift-api.onrender.com",
+    apiBaseUrl: "https://student-plaza-pa-shift-system.onrender.com",
     apiTimeoutMs: 10000,
   },
   window.PA_SHIFT_CONFIG || {}
